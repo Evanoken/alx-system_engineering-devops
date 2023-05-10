f0db84b7a1978e5dd9982e4456fc22ea389d0e4a
Evans Ngugi <95754975+Evanoken@users.noreply.github.com>
2023-05-10 19:36:18 -0400

    Updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 import requests
-
+import argparse
 
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
-                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
-    with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
-        try:
-            return info.get('data', {}).get('subscribers', 0)
-        except Exception:
-            return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "myBot/0.0.1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get the number of subscribers for a subreddit")
+    parser.add_argument("subreddit", type=str, help="The name of the subreddit")
+    args = parser.parse_args()
+
+    subscribers = number_of_subscribers(args.subreddit)
+    print("{} has {} subscribers".format(args.subreddit, subscribers))
+
