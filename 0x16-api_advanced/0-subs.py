01636931973fa26c89974a6b79bfa53424787895
Evans Ngugi <ngugie649@gmail.com>
2023-05-10 21:41:49 -0700

    This is an advanced API:

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
-import argparse
+
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "myBot/0.0.1"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Get the number of subscribers for a subreddit")
-    parser.add_argument("subreddit", type=str, help="The name of the subreddit")
-    args = parser.parse_args()
-
-    subscribers = number_of_subscribers(args.subreddit)
-    print("{} has {} subscribers".format(args.subreddit, subscribers))
-
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
